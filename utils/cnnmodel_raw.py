eb9fdd7e166f8c0b9398b5e1ed021cbceab337ce
Nathan Temiquel <ntemiquel@gmail.com>
2023-08-22 13:58:18 -0700

    initialization method

--- utils/cnnmodel_raw.py
+++ utils/cnnmodel_raw.py
@@ -16,11 +16,7 @@ from tensorflow.keras.models import Model
 with open('utils/config.yml') as file:
   config = yaml.safe_load(file)
 
-# Selected model for configurations
-mod_sel = 'cnn_raw'
-# Output path for model data
-DATA_PATH = config[mod_sel]['data_path']
-MOUT_PATH = config[mod_sel]['mout_path']
+
 
 class PredictionCallback(tf.keras.callbacks.Callback):
   '''
@@ -48,200 +44,222 @@ class PredictionCallback(tf.keras.callbacks.Callback):
       pd.DataFrame(data=self.model.predict(self.validation_data),index=self.y_dev.index).to_csv(val,mode='w')
 
 
-
-def train_cnn_model(X_train,y_train,X_dev,y_dev,hyperparameters=None,fast=True,id_val=None):
+class cnn_model:
   '''
-  Trains a CNN model using the predetermined architecture and returns the model
-
-  Args:
-    X_train: DataFrame or ndarray of training set input features
-    y_train: DataFrame or Series of labels for the X_train samples, must be in
-      the same order as X_train samples
-    X_dev: DataFrame or ndarray of dev set input features
-    y_dev: DataFrame or Series of labels for the X_dev samples, must be in the
-      same order as the X_train samples
-    hyperparameters: an array of hyperparameters in the order: learning
-      rate (lr) (default 0.0001), batch size (default 100), dropout rate (drop))
-      (default 0.55), epochs (default 10)
-    fast: boolean. if true, then the model runs more than twice as fast but does
-      not record predictions of every sample for every epoch. If False, runs
-      much more slowly but creates a csv of probability weights for every sample
-      at every epoch for both the training and validation sets
-    id_val: string that is prepended to all output files for tracking
-
-  Returns:
-      a tuple containing the trained model and the History object from training
-      that model
+  Manages CNN model, on initialization the appropriate configurations are 
+  selected depending on the model chosen
   '''
 
-  # if no hyperparameters are set, set the defaults, otherwise extract them
-  if not hyperparameters:
-    lr=config[mod_sel]['lr']
-    batch_size=config[mod_sel]['batch_size']
-    drop=config[mod_sel]['drop']
-    epochs=config[mod_sel]['epochs']
-  else:
-    lr=hyperparameters[0]
-    batch_size=hyperparameters[1]
-    drop=hyperparameters[2]
-    epochs=hyperparameters[3]
+  def __init__(self,mod_sel)
+    '''
+    Args:
+      mod_sel: string defining which model to build; determines default 
+       hyperparameters, filepaths, and structure. Options: 'cnn_raw'
+    '''
+    self.mod_sel = mod_sel
+    self.data_path = config[self.mod_sel]['data_path']
+    self.mout_path = config[self.mod_sel]['mout_path']
+    self.lr = config[self.mod_sel]['lr']
+    self.batch_size = config[self.mod_sel]['batch_size']
+    self.drop = config[self.mod_sel]['drop']
+    self.epochs = config[self.mod_sel]['epochs']
+    self.lrlu_alpha = config[self.mod_sel]['lrlu_alpha']
+    self.metrics = [config[self.mod_sel]['metrics']]
 
-  # initialize configured parameters for callbacks
-  train_out=config[mod_sel]['train_log']
-  val_out=config[mod_sel]['val_log']
-  _monitor=config[mod_sel]['monitor']
-  _min_delta=config[mod_sel]['min_delta']
-  _patience=config[mod_sel]['patience']
-  
-  # determine the appropriate callbacks, depending on if fast is true or false
-  if not fast:
-    callbacks=[PredictionCallback(X_train,X_dev,y_train,y_dev,id_val+train_out,
-                                  id_val+val_out,DATA_PATH),
-               K.callbacks.EarlyStopping(monitor=_monitor,min_delta=_min_delta,patience=_patience)]
-  else:
-    callbacks=[K.callbacks.EarlyStopping(monitor=_monitor,min_delta=_min_delta,patience=_patience)]
 
-  #call build_cnn and train model, output trained model
-  cnn_model=build_cnn(X_train.shape,y_train.max()+1,lr,drop)
+  def train_cnn_model(X_train,y_train,X_dev,y_dev,hyperparameters=None,fast=True,id_val=None):
+    '''
+    Trains a CNN model using the predetermined architecture and returns the model
 
-  cnn_hist=cnn_model.fit(X_train,y_train,batch_size=batch_size,epochs=epochs,validation_data=(X_dev,y_dev),callbacks=callbacks)
+    Args:
+      X_train: DataFrame or ndarray of training set input features
+      y_train: DataFrame or Series of labels for the X_train samples, must be in
+        the same order as X_train samples
+      X_dev: DataFrame or ndarray of dev set input features
+      y_dev: DataFrame or Series of labels for the X_dev samples, must be in the
+        same order as the X_train samples
+      hyperparameters: an array of hyperparameters in the order: learning
+        rate (lr) (default 0.0001), batch size (default 100), dropout rate (drop))
+        (default 0.55), epochs (default 10)
+      fast: boolean. if true, then the model runs more than twice as fast but does
+        not record predictions of every sample for every epoch. If False, runs
+        much more slowly but creates a csv of probability weights for every sample
+        at every epoch for both the training and validation sets
+      id_val: string that is prepended to all output files for tracking
 
-  return cnn_model,cnn_hist
+    Returns:
+        a tuple containing the trained model and the History object from training
+        that model
+    '''
 
-def layer_CBnAP(X_in,nfilters,size_C,s_C,size_P,lnum):
-  '''Defines one set of convolutional layers with a Convolution,
-  BatchNormalization, LeakyReLU activation, and MaxPooling1D
-
-  Args:
-    X_in: input matrix for the layers
-    nfilters: number of filters for the convolutional layer
-    size_C: size of the convolutional kernel
-    s_C: step size for the convolution layer
-    size_P: size for MaxPooling layer
-    lnum: layer number used for debugging
-
-  Returns:
-    Matrix of output values from this set of layers
-  '''
-
-  # CONV -> BN -> RELU -> MaxPooling Block applied to X
-  X_working=Conv1D(nfilters,size_C,s_C,name='conv'+lnum)(X_in)
-  X_working=BatchNormalization(name='bn'+lnum)(X_working)
-  X_working=LeakyReLU(alpha=config[mod_sel]['lrlu_alpha'],name='relu'+lnum)(X_working)
-  X_working=MaxPooling1D(size_P,name='mpool'+lnum)(X_working)
-  return X_working
-
-def build_cnn(X_shape,y_shape,lr=0.001,drop=0.55):
-  '''Defines and builds the CNN model with the given inputs
-
-  Args:
-    X_shape: the shape of the data for the model
-    y_shape: the shape of the labels for the model
-    lr: learning rate, default 0.001
-    drop: drop rate, default 0.55
-
-  Returns:
-    a compiled model as defined by this method
-  '''
-
-  # Define the input placeholder as a tensor with the shape of the features
-  #this data has one-dimensional data with no channels
-  X_input=Input((X_shape[1],1))
-
-  #first layer - conv, batch normalization, activation, pooling
-  nfilters=16
-  size_C=21
-  s_C=1
-  size_P=2
-  X=layer_CBnAP(X_input,nfilters,size_C,s_C,size_P,'1')
-
-  #second layer - conv, batch normalization, activation, pooling
-  nfilters=32
-  size_C=11
-  s_C=1
-  size_P=2
-  X=layer_CBnAP(X,nfilters,size_C,s_C,size_P,'2')
-
-  #third layer - conv, batch normalization, activation, pooling
-  nfilters=64
-  size_C=5
-  s_C=1
-  size_P=2
-  X=layer_CBnAP(X,nfilters,size_C,s_C,size_P,'3')
-
-  #flatten for final layers
-  X=Flatten()(X)
-
-  #layer 4 - fully connected layer 1 dense,Batch normalization,activation,dropout
-  X=Dense(2048, use_bias=False,name='dense4')(X)
-  X=BatchNormalization(name='bn4')(X)
-  X=Activation("tanh",name='tanh4')(X)
-  X=Dropout(drop,name='dropout4')(X)
-
-  #layer 5 - fully connected layer 2 dense, batch normalization, softmax output
-  X=Dense(y_shape,use_bias=False,name='dense5')(X)
-  X=BatchNormalization(name='bn5')(X)
-  outputs=Activation("softmax",name='softmax5')(X)
-
-  model=Model(inputs=X_input,outputs=outputs)
-
-  opt=K.optimizers.RMSprop(learning_rate=lr)
-  #opt=K.optimizers.Nadam(lr)
-  model.summary()
-  model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(),optimizer=opt,metrics=config[mod_sel]['metrics'])
-  return model
-
-def test_cnn_model(model,X_test,y_test,id_val='0',test=True,threshold=0.95,fast=False):
-  """
-  test a trained model with given parameters, creates a csv of confusion matrix
-  at Model Data/CNN Model/ 'id_val'+comfmatout.csv
-
-  Args:
-    model: a trained keras model used to predict the categories of the test set
-    X_test: a DataFrame or ndarray of sample features sets for testing
-    y_test: a DataFrame or Series of sample labels the X_test feature set - must
-      be in the same order as the X_test feature set
-    id_val: a string used in the file name of the outputs for identification
-
-  Returns:
-    None; creates a file at the /Model Data/CNN Model/Raw/ folder
-  """
-  #predict classes for provided test set
-  _batch_size=config[mod_sel]['test_batch']
-  y_pred=model.predict(X_test,batch_size=_batch_size)
-
-  #report confusion matrix
-  confmat=build_confmat(y_test,y_pred,threshold)
-  display(confmat)
-
-  #if fast is not True, save the confusion matrix as either test or validation
-  if not fast:
-    if test:
-      id_val=id_val+'_test'
+    # if no hyperparameters are set, set the defaults, otherwise extract them
+    if not hyperparameters:
+      lr=config[mod_sel]['lr']
+      batch_size=config[mod_sel]['batch_size']
+      drop=config[mod_sel]['drop']
+      epochs=config[mod_sel]['epochs']
     else:
-      id_val=id_val+'_validation'
-    #save confusion matrix as csv to drive
-    confmatout_path=DATA_PATH+id_val
+      lr=hyperparameters[0]
+      batch_size=hyperparameters[1]
+      drop=hyperparameters[2]
+      epochs=hyperparameters[3]
 
-    confmat.to_csv(confmatout_path+r'_confmat.csv')
-    #save output weights
-    pd.DataFrame(data=model.predict(X_test),index=y_test.index.values).to_csv(confmatout_path+'_probs.csv')
+    # initialize configured parameters for callbacks
+    train_out=config[mod_sel]['train_log']
+    val_out=config[mod_sel]['val_log']
+    _monitor=config[mod_sel]['monitor']
+    _min_delta=config[mod_sel]['min_delta']
+    _patience=config[mod_sel]['patience']
+    
+    # determine the appropriate callbacks, depending on if fast is true or false
+    if not fast:
+      callbacks=[PredictionCallback(X_train,X_dev,y_train,y_dev,id_val+train_out,
+                                    id_val+val_out,DATA_PATH),
+                K.callbacks.EarlyStopping(monitor=_monitor,min_delta=_min_delta,patience=_patience)]
+    else:
+      callbacks=[K.callbacks.EarlyStopping(monitor=_monitor,min_delta=_min_delta,patience=_patience)]
 
-def save_model(model,mout_path):
-  '''saves model data to the given output mout_path
+    #call build_cnn and train model, output trained model
+    cnn_model=build_cnn(X_train.shape,y_train.max()+1,lr,drop)
 
-  Args:
-    model: the model history file
-    mout_path: the filepath to store the model dataframe
+    cnn_hist=cnn_model.fit(X_train,y_train,batch_size=batch_size,epochs=epochs,validation_data=(X_dev,y_dev),callbacks=callbacks)
 
-  Returns:
-    None. Creates a file at the given filepath
-  '''
+    return cnn_model,cnn_hist
 
-  model.save(mout_path+'cnn.h5')
-  print('model saved')
+  def layer_CBnAP(X_in,nfilters,size_C,s_C,size_P,lnum):
+    '''Defines one set of convolutional layers with a Convolution,
+    BatchNormalization, LeakyReLU activation, and MaxPooling1D
 
-def dec_pred(y_pred,threshold=0.95):
+    Args:
+      X_in: input matrix for the layers
+      nfilters: number of filters for the convolutional layer
+      size_C: size of the convolutional kernel
+      s_C: step size for the convolution layer
+      size_P: size for MaxPooling layer
+      lnum: layer number used for debugging
+
+    Returns:
+      Matrix of output values from this set of layers
+    '''
+
+    # CONV -> BN -> RELU -> MaxPooling Block applied to X
+    X_working=Conv1D(nfilters,size_C,s_C,name='conv'+lnum)(X_in)
+    X_working=BatchNormalization(name='bn'+lnum)(X_working)
+    X_working=LeakyReLU(alpha=config[mod_sel]['lrlu_alpha'],name='relu'+lnum)(X_working)
+    X_working=MaxPooling1D(size_P,name='mpool'+lnum)(X_working)
+    return X_working
+
+  def build_cnn(X_shape,y_shape,lr=0.001,drop=0.55):
+    '''Defines and builds the CNN model with the given inputs
+
+    Args:
+      X_shape: the shape of the data for the model
+      y_shape: the shape of the labels for the model
+      lr: learning rate, default 0.001
+      drop: drop rate, default 0.55
+
+    Returns:
+      a compiled model as defined by this method
+    '''
+
+    # Define the input placeholder as a tensor with the shape of the features
+    #this data has one-dimensional data with no channels
+    X_input=Input((X_shape[1],1))
+
+    #first layer - conv, batch normalization, activation, pooling
+    nfilters=16
+    size_C=21
+    s_C=1
+    size_P=2
+    X=layer_CBnAP(X_input,nfilters,size_C,s_C,size_P,'1')
+
+    #second layer - conv, batch normalization, activation, pooling
+    nfilters=32
+    size_C=11
+    s_C=1
+    size_P=2
+    X=layer_CBnAP(X,nfilters,size_C,s_C,size_P,'2')
+
+    #third layer - conv, batch normalization, activation, pooling
+    nfilters=64
+    size_C=5
+    s_C=1
+    size_P=2
+    X=layer_CBnAP(X,nfilters,size_C,s_C,size_P,'3')
+
+    #flatten for final layers
+    X=Flatten()(X)
+
+    #layer 4 - fully connected layer 1 dense,Batch normalization,activation,dropout
+    X=Dense(2048, use_bias=False,name='dense4')(X)
+    X=BatchNormalization(name='bn4')(X)
+    X=Activation("tanh",name='tanh4')(X)
+    X=Dropout(drop,name='dropout4')(X)
+
+    #layer 5 - fully connected layer 2 dense, batch normalization, softmax output
+    X=Dense(y_shape,use_bias=False,name='dense5')(X)
+    X=BatchNormalization(name='bn5')(X)
+    outputs=Activation("softmax",name='softmax5')(X)
+
+    model=Model(inputs=X_input,outputs=outputs)
+
+    opt=K.optimizers.RMSprop(learning_rate=lr)
+    #opt=K.optimizers.Nadam(lr)
+    model.summary()
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(),optimizer=opt,metrics=config[mod_sel]['metrics'])
+    return model
+
+  def test_cnn_model(model,X_test,y_test,id_val='0',test=True,threshold=0.95,fast=False):
+    """
+    test a trained model with given parameters, creates a csv of confusion matrix
+    at Model Data/CNN Model/ 'id_val'+comfmatout.csv
+
+    Args:
+      model: a trained keras model used to predict the categories of the test set
+      X_test: a DataFrame or ndarray of sample features sets for testing
+      y_test: a DataFrame or Series of sample labels the X_test feature set - must
+        be in the same order as the X_test feature set
+      id_val: a string used in the file name of the outputs for identification
+
+    Returns:
+      None; creates a file at the /Model Data/CNN Model/Raw/ folder
+    """
+    #predict classes for provided test set
+    _batch_size=config[mod_sel]['test_batch']
+    y_pred=model.predict(X_test,batch_size=_batch_size)
+
+    #report confusion matrix
+    confmat=build_confmat(y_test,y_pred,threshold)
+    display(confmat)
+
+    #if fast is not True, save the confusion matrix as either test or validation
+    if not fast:
+      if test:
+        id_val=id_val+'_test'
+      else:
+        id_val=id_val+'_validation'
+      #save confusion matrix as csv to drive
+      confmatout_path=DATA_PATH+id_val
+
+      confmat.to_csv(confmatout_path+r'_confmat.csv')
+      #save output weights
+      pd.DataFrame(data=model.predict(X_test),index=y_test.index.values).to_csv(confmatout_path+'_probs.csv')
+
+  def save_model(model,mout_path):
+    '''saves model data to the given output mout_path
+
+    Args:
+      model: the model history file
+      mout_path: the filepath to store the model dataframe
+
+    Returns:
+      None. Creates a file at the given filepath
+    '''
+
+    model.save(mout_path+'cnn.h5')
+    print('model saved')
+
+  def dec_pred(y_pred,threshold=0.95):
   """takes prediction weights and applies a decision threshold to deterime the
   predicted class for each sample
 
